8b6c4b96ba6764d367ac059d8fbc2d173236be8b
JafirRehman <131444360+JafirRehman@users.noreply.github.com>
2024-02-28 20:04:00 +0500

    Add files via upload

---
+++ react/react_advance_1/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Randomgif from './components/Randomgif'
+import Specialgif from './components/Specialgif'
+
+const App = () => {
+  return (
+    <div>
+      <Randomgif/>
+      <Specialgif/>
+    </div>
+  )
+}
+
+export default App
